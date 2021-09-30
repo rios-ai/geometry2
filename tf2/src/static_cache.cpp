c9888dc087d50dd0b75571363ffea71f3639be40
Leo Keselman <leo.keselman@rios.ai>
2021-09-30 15:08:00 -0700

    Compiling version with better extrapolation issue enunciation

--- tf2/src/static_cache.cpp
+++ tf2/src/static_cache.cpp
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of the Willow Garage, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -58,7 +58,7 @@ void StaticCache::clearList() { return; };
 
 unsigned int StaticCache::getListLength() {   return 1; };
 
-CompactFrameID StaticCache::getParent(ros::Time time, std::string* error_str)
+CompactFrameID StaticCache::getParent(ros::Time time, std::string* error_str, CompactFrameID& error_frame, CompactFrameID& error_frame_child)
 {
   return storage_.frame_id_;
 }
@@ -68,13 +68,13 @@ P_TimeAndFrameID StaticCache::getLatestTimeAndParent()
   return std::make_pair(ros::Time(), storage_.frame_id_);
 }
 
-ros::Time StaticCache::getLatestTimestamp() 
-{   
+ros::Time StaticCache::getLatestTimestamp()
+{
   return ros::Time();
 };
 
-ros::Time StaticCache::getOldestTimestamp() 
-{   
+ros::Time StaticCache::getOldestTimestamp()
+{
   return ros::Time();
 };
 
